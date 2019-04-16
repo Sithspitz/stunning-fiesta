dcdc76a723965dc63a3e1f08c034b0623a0b0dca
Richard Buchanan <rjb590@bham.ac.uk>
2019-04-16 15:17:23 +0100

    Gene expression and heatmap creation version 1.

--- R_scripts/Analyse_Data_V1.0/RNA_Expression/expression_signatures.R
+++ R_scripts/Analyse_Data_V1.0/RNA_Expression/expression_signatures.R
@@ -1,4 +1,5 @@
-### Lung Adenocarcinoma PanCancer Atlas RNA Expression Playing Around ###
+### Lung Adenocarcinoma PanCancer Atlas RNA Expression ###
+## Should be adopted dependent on the analysis in question ##
 source("./R_scripts/Functions/functions.R")
 source("./R_scripts/Functions/GeneSets.R")
 setwd("~/DataShare/TCGA_RNA_Analysis/Testing/")
@@ -6,40 +7,133 @@ rna <- read.csv("luad_tcga_pancancer_rna.csv", header = T)
 
 
 
-
-# Label Gene Signatures
-Class2genes <- c("HLA-DOB", "HLA-DQA1", "HLA-DQA2", "HLA-DRB5", "HLA-DPB1",
-                 "HLA-DMB", "HLA-DRA", "HLA-DMA", "HLA-DOA")
-
-rna$Class2 <- ifelse(
-  (rna$Hugo_Symbol %in% Class2genes), T, F)
-
+# Label Immune Gene Signatures
+rna$aDC <- ifelse(
+  (rna$Hugo_Symbol %in% aDC_genes), T, F)
+rna$B_Cells <- ifelse(
+  (rna$Hugo_Symbol %in% BCells_genes), T, F)
+rna$CD8 <- ifelse(
+  (rna$Hugo_Symbol %in% CD8_genes), T, F)
+rna$Cytotoxic <- ifelse(
+  (rna$Hugo_Symbol %in% Cytotoxic_genes), T, F)
+rna$DC <- ifelse(
+  (rna$Hugo_Symbol %in% DC_genes), T, F)
+rna$Eosinophils <- ifelse(
+  (rna$Hugo_Symbol %in% Eosinophils_genes), T, F)
+rna$iDC <- ifelse(
+  (rna$Hugo_Symbol %in% iDC_genes), T, F)
+rna$Macrophages <- ifelse(
+  (rna$Hugo_Symbol %in% Macrophages_genes), T, F)
+rna$Mast_Cells <- ifelse(
+  (rna$Hugo_Symbol %in% MastCells_genes), T, F)
+rna$Neutrophils <- ifelse(
+  (rna$Hugo_Symbol %in% Neutrophils_genes), T, F)
+rna$NK <- ifelse(
+  (rna$Hugo_Symbol %in% NK_genes), T, F)
+rna$CD56_Bright_NK <- ifelse(
+  (rna$Hugo_Symbol %in% NKCD56bright_genes), T, F)
+rna$CD56_Dim_NK <- ifelse(
+  (rna$Hugo_Symbol %in% NKCD56dim_genes), T, F)
+rna$Tcm <- ifelse(
+  (rna$Hugo_Symbol %in% Tcm_genes), T, F)
+rna$Tem <- ifelse(
+  (rna$Hugo_Symbol %in% Tem_genes), T, F)
+rna$Tfh <- ifelse(
+  (rna$Hugo_Symbol %in% Tfh_genes), T, F)
+rna$Tgd <- ifelse(
+  (rna$Hugo_Symbol %in% Tgd_genes), T, F)
+rna$Th1 <- ifelse(
+  (rna$Hugo_Symbol %in% Th1_genes), T, F)
 rna$Th17 <- ifelse(
   (rna$Hugo_Symbol %in% Th17_genes), T, F)
+rna$Th2 <- ifelse(
+  (rna$Hugo_Symbol %in% Th2_genes), T, F)
+rna$Tregs <- ifelse(
+  (rna$Hugo_Symbol %in% Tregs_genes), T, F)
 
-class2datatemp <- CalGeneMeanRNASeqZScore(rna, Class2)
-colnames(class2datatemp)[colnames(class2datatemp)=="value"] <- "Class_2_mean"
-
-th17datatemp <- CalGeneMeanRNASeqZScore(rna, Th17)
-colnames(th17datatemp)[colnames(th17datatemp)=="value"] <- "Th17_mean"
+# Calculate mean scores for given genesets
+aDCdata <- CalGeneMeanRNASeqZScore(rna, aDC)
+colnames(aDCdata)[colnames(aDCdata)=="value"] <- "aDC_Mean"
+Bcelldata <- CalGeneMeanRNASeqZScore(rna, B_Cells)
+colnames(Bcelldata)[colnames(Bcelldata)=="value"] <- "B_Cell_Mean"
+CD8data <- CalGeneMeanRNASeqZScore(rna, CD8)
+colnames(CD8data)[colnames(CD8data)=="value"] <- "CD8_Mean"
+Cytotoxicdata <- CalGeneMeanRNASeqZScore(rna, Cytotoxic)
+colnames(Cytotoxicdata)[colnames(Cytotoxicdata)=="value"] <- "Cytotoxic_Mean"
+DCdata <- CalGeneMeanRNASeqZScore(rna, DC)
+colnames(DCdata)[colnames(DCdata)=="value"] <- "DC_Mean"
+Eosinophilsdata <- CalGeneMeanRNASeqZScore(rna, Eosinophils)
+colnames(Eosinophilsdata)[colnames(Eosinophilsdata)=="value"] <- "Eosinophil_Mean"
+iDCdata <- CalGeneMeanRNASeqZScore(rna, iDC)
+colnames(iDCdata)[colnames(iDCdata)=="value"] <- "iDC_Mean"
+Macrophagesdata <- CalGeneMeanRNASeqZScore(rna, Macrophages)
+colnames(Macrophagesdata)[colnames(Macrophagesdata)=="value"] <- "Macrophage_Mean"
+Mastcelldata <- CalGeneMeanRNASeqZScore(rna, Mast_Cells)
+colnames(Mastcelldata)[colnames(Mastcelldata)=="value"] <- "Mast_Cell_Mean"
+Neutrophilsdata <- CalGeneMeanRNASeqZScore(rna, Neutrophils)
+colnames(Neutrophilsdata)[colnames(Neutrophilsdata)=="value"] <- "Neutrophil_Mean"
+NKdata <- CalGeneMeanRNASeqZScore(rna, NK)
+colnames(NKdata)[colnames(NKdata)=="value"] <- "NK_Mean"
+NKCD56brightdata <- CalGeneMeanRNASeqZScore(rna, CD56_Bright_NK)
+colnames(NKCD56brightdata)[colnames(NKCD56brightdata)=="value"] <- "CD56_Bright_NK_Mean"
+NKCD56dimdata <- CalGeneMeanRNASeqZScore(rna, CD56_Dim_NK)
+colnames(NKCD56dimdata)[colnames(NKCD56dimdata)=="value"] <- "CD56_Dim_NK_Mean"
+Tcmdata <- CalGeneMeanRNASeqZScore(rna, Tcm)
+colnames(Tcmdata)[colnames(Tcmdata)=="value"] <- "Tcm_Mean"
+Temdata <- CalGeneMeanRNASeqZScore(rna, Tem)
+colnames(Temdata)[colnames(Temdata)=="value"] <- "Tem_Mean"
+Tfhdata <- CalGeneMeanRNASeqZScore(rna, Tfh)
+colnames(Tfhdata)[colnames(Tfhdata)=="value"] <- "Tfh_Mean"
+Tgddata <- CalGeneMeanRNASeqZScore(rna, Tgd)
+colnames(Tgddata)[colnames(Tgddata)=="value"] <- "Tgd_Mean"
+Th1data <- CalGeneMeanRNASeqZScore(rna, Th1)
+colnames(Th1data)[colnames(Th1data)=="value"] <- "Th1_Mean"
+Th17data <- CalGeneMeanRNASeqZScore(rna, Th17)
+colnames(Th17data)[colnames(Th17data)=="value"] <- "Th17_Mean"
+Th2data <- CalGeneMeanRNASeqZScore(rna, Th2)
+colnames(Th2data)[colnames(Th2data)=="value"] <- "Th2_Mean"
+Tregsdata <- CalGeneMeanRNASeqZScore(rna, Tregs)
+colnames(Tregsdata)[colnames(Tregsdata)=="value"] <- "Treg_Mean"
 
 # Merge
-expsig1 <- merge(th17datatemp, class2datatemp, by = "Patient.ID")
+m1 <- merge(aDCdata, Bcelldata, by = "Patient.ID")
+m2 <- merge(m1, CD8data, by = "Patient.ID")
+m3 <- merge(m2, Cytotoxicdata, by = "Patient.ID")
+m4 <- merge(m3, DCdata, by = "Patient.ID")
+m5 <- merge(m4, Eosinophilsdata, by = "Patient.ID")
+m6 <- merge(m5, iDCdata, by = "Patient.ID")
+m7 <- merge(m6, Macrophagesdata, by = "Patient.ID")
+m8 <- merge(m7, Mastcelldata, by = "Patient.ID")
+m9 <- merge(m8, Neutrophilsdata, by = "Patient.ID")
+m10 <- merge(m9, NKdata, by = "Patient.ID")
+m11 <- merge(m10, NKCD56brightdata, by = "Patient.ID")
+m12 <- merge(m11, NKCD56dimdata, by = "Patient.ID")
+m13 <- merge(m12, Tcmdata, by = "Patient.ID")
+m14 <- merge(m13, Temdata, by = "Patient.ID")
+m15 <- merge(m14, Tfhdata, by = "Patient.ID")
+m16 <- merge(m15, Tgddata, by = "Patient.ID")
+m17 <- merge(m16, Th1data, by = "Patient.ID")
+m18 <- merge(m17, Th17data, by = "Patient.ID")
+m19 <- merge(m18, Th2data, by = "Patient.ID")
+m20 <- merge(m19, Tregsdata, by = "Patient.ID")
+rm(m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, 
+   m12, m13, m14, m15, m16, m17, m18, m19)
 
-# Basic Heatmap
-heat1 <- expsig1 %>% gather(-contains("Patient.ID"), key = "Geneset", value = "Zscore")
+# Heatmap Organisation
+heat1 <- m20 %>% gather(-contains("Patient.ID"), key = "Geneset", value = "Zscore")
 Bound2 <- acast(heat1, Patient.ID ~ Geneset, value.var = "Zscore")
 mxCell <- t(Bound2)
 xx <- mxCell[rowSums(!is.na(mxCell))!=0, colSums(!is.na(mxCell))!=0] 
 
+# Heatmap Clustering 
 hr <- hclust(as.dist(1-cor((xx), method="pearson")), method="complete")
 mycl <- cutree(hr, h=max(hr$height/1.12))
 clusterRows <- rainbow(length(unique(mycl)))
 myClusterSideBar <- clusterRows[mycl]
-my_palette <- colorRampPalette(c("blue", "black", "gold"))
+my_palette <- colorRampPalette(c("blue", "white", "red"))(n = 299)
 
-heatmap.2(xx, 
-          main = "Hierarchical Clustering of Gene Pathways",
+cairo_pdf("./heatmap_1.pdf")
+heatmap.2(xx,
           #Rowv = NA,
           Colv = as.dendrogram(hr),
           #dendrogram = "col",
@@ -48,28 +142,23 @@ heatmap.2(xx,
           density.info = "none",
           trace = "none",
           ColSideColors = myClusterSideBar,
-          margins = c(12,12))
+          key = F,
+          labCol = F,
+          margins = c(13,12))
+dev.off()
+
 
 # Output with the correct clusters
 test <- heat1
 test1 <- spread(test, key = "Geneset", value = "Zscore")
 test1$cluster <- mycl
+write.csv(test1, "luad_pancancer_total_immune_gene_clusters.csv", row.names = F)
 
 
-# Doing a Linear Discriminant Analysis (LDA)
+
+# Not currently used: Doing a Linear Discriminant Analysis (LDA)
 ## dlyr note, if want to positively select then: 
 ### e.g. attrich <- expsig1 %>% dplyr:: select(contains(".ID"))
-att2 <- expsig1 %>% dplyr:: select(-contains(".ID"))
-att1 <- att2 %>% dplyr:: select(-contains("Subtype"))
-
-att1[is.na(att1)] <- 0
-pca <- prcomp(att1[,-1],
-              center = TRUE,
-              scale. = TRUE) 
-tmp <- cor(att1[,-1])
-tmp[upper.tri(tmp)] <- 0
-diag(tmp) <- 0
-data.new <- att2[,!apply(tmp,2,function(x) any(abs(x) > 0.945))]
-
-
+# att2 <- expsig1 %>% dplyr:: select(-contains(".ID"))
+# att1 <- att2 %>% dplyr:: select(-contains("Subtype"))
 
--- R_scripts/Functions/GeneSets.R
+++ R_scripts/Functions/GeneSets.R
@@ -5,7 +5,7 @@ source("./R_scripts/Functions/functions.R")
 
 # Import Pre-made gene sets #
 
-# Tested with "Th17_geneset"
+# All .csv files in the directory
 predir <- c("./R_scripts/Gene_Sets/")
 setwd(predir)
 temp <- list.files(pattern="*.csv")
@@ -13,6 +13,26 @@ list2env(
   lapply(setNames(temp, make.names(gsub("*.csv$", "", temp))), 
          read.csv), envir = .GlobalEnv)
 
-# Clean the geneset as characters
+# Clean the immune genesets as characters
+aDC_genes <- takegenelevels(aDC_geneset)
+BCells_genes <- takegenelevels(BCells_geneset)
+CD8_genes <- takegenelevels(CD8_geneset)
+Cytotoxic_genes <- takegenelevels(Cytotoxic_geneset)
+DC_genes <- takegenelevels(DC_geneset)
+Eosinophils_genes <- takegenelevels(Eosinophils_geneset)
+iDC_genes <- takegenelevels(iDC_geneset)
+Macrophages_genes <- takegenelevels(Macrophages_geneset)
+MastCells_genes <- takegenelevels(MastCells_geneset)
+Neutrophils_genes <- takegenelevels(Neutrophils_geneset)
+NK_genes <- takegenelevels(NK_geneset)
+NKCD56bright_genes <- takegenelevels(NKCD56bright_geneset)
+NKCD56dim_genes <- takegenelevels(NKCD56dim_geneset)
+Tcm_genes <- takegenelevels(Tcm_geneset)
+Tem_genes <- takegenelevels(Tem_geneset)
+Tfh_genes <- takegenelevels(Tfh_geneset)
+Tgd_genes <- takegenelevels(Tgd_geneset)
+Th1_genes <- takegenelevels(Th1_geneset)
 Th17_genes <- takegenelevels(Th17_geneset)
+Th2_genes <- takegenelevels(Th2_geneset)
+Tregs_genes <- takegenelevels(Tregs_geneset)
 
