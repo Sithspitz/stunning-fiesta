b3ad7e1b9563df3788002d88dd457ff3a2485474
Richard Buchanan <rjb590@bham.ac.uk>
2019-04-25 11:51:11 +0100

    Intermediated commit using DropLevels - for writing out properly this afternoon!

--- R_scripts/Analyse_Data_V1.0/RNA_Expression/enrichment_analysis.R
+++ R_scripts/Analyse_Data_V1.0/RNA_Expression/enrichment_analysis.R
@@ -10,4 +10,14 @@ source("./R_scripts/Functions/functions.R")
 
 setwd("~/DataShare/TCGA_RNA_Analysis/Input/")
 rna <- read.csv("luad_tcga_pancancer_rna.csv", header = T)
+mut2 <- read.csv("KRAS_STK11_plus_dbl_tidy_vs_other.csv", header = T)
 
+
+mergev1 <- merge(rna, mut2, by = "Patient.ID")
+mergev2 <- mergev1
+mergev3 <- mergev1
+mergev4 <- mergev1
+extracting_other_muts <- mergev3[mergev3 == "Other_Mut"]
+
+Intermediate <- droplevels(subset(mergev4, Mutation_Status != "Other_Mut"))
+### THIS DROPLEVELS SCRIPT REALLY IS GOD HERE, WRITE UP PROPERLY WITH THIS SCRIPT!! ###
