77645236043860e75da3de52c37910377221b2b0
Richard Buchanan <rjb590@bham.ac.uk>
2019-05-03 13:02:21 +0100

    Commit with notes for next week work.

--- R_scripts/Analyse_Data_V1.0/RNA_Expression/enrichment_analysis.R
+++ R_scripts/Analyse_Data_V1.0/RNA_Expression/enrichment_analysis.R
@@ -21,34 +21,12 @@ Intermediate4 <- Intermediate3
 Intermediate4$DataType <- NULL
 
 
+### I have managed to process the data to how I want it 
+### But need to manipulate it futher to ensure that the input is correct for GSVA
+### Is there anyway of doing this without losing information?
+### Perhaps look at creating an ExpressionSet object to keep metadata?
+### Need to transpose the data into the correct data.matrix in paper
 
 
 
-
-#### TEST GVSA WITH FAKE DATA ####
-## This is the non-S2 gene set enriched example ##
-not_s2_enriched_1 <- read.csv("test_gene_set.csv", header = T, row.names = 1)
-not_s2_enriched_2 <- data.matrix(not_s2_enriched_1) ## has to be a matrix
-gmt_gene_set <- getGmt("test_enrich_Set_gmt_idea.txt") ## Using a gmt gene set
-
-not_s2_enriched_output <- gsva(not_s2_enriched_2, gmt_gene_set, method = "gsva")
-
-## This is the S2 gene set enriched example ##
-s2_enriched_1 <- read.csv("test_gene_set_s2_enriched.csv", header = T, row.names = 1)
-s2_enriched_2 <- data.matrix(s2_enriched_1)
-
-s2_enriched_output <- gsva(s2_enriched_2, gmt_gene_set, method = "gsva")
-s2_enriched_output_ssgsea <- gsva(s2_enriched_2, gmt_gene_set, method = "ssgsea")
-
-## This is the S2 high and S3 low gene set enriched example ##
-
-s2_hi_s3_lo_1 <- read.csv("test_gene_set_s2_enriched_s3_low.csv", header = T, row.names = 1)
-s2_hi_s3_lo_1 <- data.matrix(s2_hi_s3_lo_1)
-
-s2_hi_s3_lo_output <- gsva(s2_hi_s3_lo_11, gmt_gene_set, method = "gsva")
-s2_hi_s3_lo_output_ssgsea <- gsva(s2_hi_s3_lo_1, gmt_gene_set, method = "ssgsea")
-
-
-#### FINISHED END OF LAST WEEK BUT WHAT DO THESE VALUES ACTUALLY MEAN? 
-#### AND WHAT ON EARTH DO I DO NEXT...?
 
