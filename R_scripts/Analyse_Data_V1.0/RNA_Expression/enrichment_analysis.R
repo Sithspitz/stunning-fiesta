b61dc75b7eb8afc22b4e14d08bd23471726d61e2
Richard Buchanan <rjb590@bham.ac.uk>
2019-05-09 17:28:48 +0100

    Commit testing dcast as a way of transposing this data.frame

--- R_scripts/Analyse_Data_V1.0/RNA_Expression/enrichment_analysis.R
+++ R_scripts/Analyse_Data_V1.0/RNA_Expression/enrichment_analysis.R
@@ -2,6 +2,7 @@
 ## Should be adopted dependent on the analysis in question ##
 mypackages <- c("GSEABase", "GSVA", "Biobase", "genefilter",
                 "limma", "RColorBrewer", "GSVAdata")
+# INSTALL reshape PACKAGE?!?
 lapply(mypackages, library, character.only = T)
 source("./R_scripts/Functions/functions.R")
 
@@ -19,6 +20,40 @@ Intermediate2$Entrez_Gene_Id <- NULL
 Intermediate3 <- droplevels(subset(Intermediate2, DataType == "RNASeqEL"))
 Intermediate4 <- Intermediate3
 Intermediate4$DataType <- NULL
+Intermediate5 <- Intermediate4
+
+# Subset just the Mut and the WT and drop the Mutation_Status column
+my_Mut_subset <- droplevels(subset(Intermediate5, Mutation_Status == "Mut"))
+my_WT_subset <- droplevels(subset(Intermediate5, Mutation_Status == "Other_Mut"))
+my_Mut_subset2 <- my_Mut_subset
+my_WT_subset2 <- my_WT_subset
+
+my_Mut_subset2$Mutation_Status <- NULL
+my_WT_subset2$Mutation_Status <- NULL
+
+
+# Tidyr idea
+my_Mut_subset3 <- my_Mut_subset2
+
+
+t2 <- dcast(my_Mut_subset3, Patient.ID ~ Hugo_Symbol, value.var = "Value")
+
+#### TRY AND GET THE DCAST TO WORK!!!!!! 
+
+# t1 <- melt(my_Mut_subset3, id.vars =c("Patient.ID", "Hugo_Symbol"), 
+  #         measure.vars = "Value")
+
+
+
+
+
+#my_Mut_subset3 %>% spread(key = Hugo_Symbol, value = Value)
+
+#my_Mut_subset4 <- rowid_to_column(my_Mut_subset3)
+
+# TEST RESHAPE WITH MUT SUBSET
+#md <- melt(my_Mut_subset2, id=(c("Patient.ID", "Hugo_Symbol")))
+
 
 
 ### I have managed to process the data to how I want it 
