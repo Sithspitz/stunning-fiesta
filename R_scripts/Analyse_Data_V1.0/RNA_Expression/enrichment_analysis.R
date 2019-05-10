62b976e09170cb8ce9f7925cc5d89632c948c3c2
Richard Buchanan (PhD Immuno Rjb590 Immunot FT (A300)) <RJB590@student.bham.ac.uk>
2019-05-10 14:01:24 +0100

    A script for restructing that appears to work!

--- R_scripts/Analyse_Data_V1.0/RNA_Expression/enrichment_analysis.R
+++ R_scripts/Analyse_Data_V1.0/RNA_Expression/enrichment_analysis.R
@@ -34,12 +34,42 @@ my_WT_subset2$Mutation_Status <- NULL
 
 # Tidyr idea
 my_Mut_subset3 <- my_Mut_subset2
-my_factored_MT_subset <- my_Mut_subset3
+dofactor <- c("Patient.ID", "Hugo_Symbol")
+my_factored <- factorthese(my_Mut_subset3, dofactor)
+
+my_extra <- my_factored
+my_extra$extra <- as.factor(paste(my_extra$Patient.ID, my_extra$Hugo_Symbol, sep = ","))
+
+dups <- my_extra
+dropped <- dups[!duplicated(dups$extra), ]
+dropped$extra <- NULL
+
+t1 <- recast(dropped, Patient.ID ~ Hugo_Symbol, id.var = 1:2)
+write.csv(t1, "t1_please.csv")
+
+
+
+
+
+
+
+
+no_pid <- my_factored
+no_pid$Patient.ID <- NULL
+
+no_pid_spread <- spread(no_pid, Hugo_Symbol, Value)
+
+
+
+my_factored %>% group_by(Hugo_Symbol) %>% mutate(grouped_id = row_number())
+my_factored %>% spread(Patient.ID, Hugo_Symbol) %>% select(-grouped_id)
+
+my_spread <- spread(my_factored, Hugo_Symbol, Value)
 
 melted <- melt(my_Mut_subset3, id.vars = c("Patient.ID"))
 t2 <- dcast(my_Mut_subset3, Patient.ID ~ Value, value.var = "Value")
 
-write.csv(t2, "test_dcast_output_2.csv")
+write.csv(t1, "t1_please.csv")
 
 t2 <- dcast(my_Mut_subset3, Patient.ID ~ Hugo_Symbol, value.var = "Value")
 
