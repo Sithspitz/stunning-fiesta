7c6c53efc1276348ae593541ca91fecab801b4c0
Richard Buchanan (PhD Immuno Rjb590 Immunot FT (A300)) <RJB590@student.bham.ac.uk>
2019-05-21 14:58:51 +0100

    DLBCL Read script

---
+++ R_scripts/Read_Data_V1.0/dlbcread_tcga_pancancer.R
@@ -0,0 +1,38 @@
+### DLBCL Read/Pre-proccessing ###
+# Key commands
+source("./R_scripts/Functions/functions.R")
+setwd("M:/Richard B/TCGA_data/DLBCL/raw")
+
+
+
+## RNA-Seq ##
+# RSEM - RPKM EL
+RNAEL1 <- read.csv("data_RNA_Seq_v2_expression_median.csv", header = T)
+
+# Remove patients with NA
+## Generally fix NA
+RNAEL1[RNAEL1 == "#DIV/0!" | RNAEL1 == "NaN" | RNAEL1 == "#N/A" | RNAEL1 == "#VALUE!"] <- NA
+
+# Keeps columns where NA is = 0
+RNAEL2 <- RNAEL1[,colMeans(is.na(RNAEL1)) == 0]
+
+# Convert all columns but Hugo_Symbol to numerics
+Leave <- c("Hugo_Symbol")
+selections <- !names(RNAEL2) %in% Leave
+RNAEL2[,selections] <- as.numeric(as.character(unlist(RNAEL2[,selections])))
+RNAEL3 <- droplevels(RNAEL2)
+
+# Collate patients into a column called Patient.ID and values into 'RNASeqEL'
+RNAEL4 <- RNAEL3 %>% gather(contains("TCGA"), key = "Patient.ID", value = "RNASeqEL")
+
+# Factor the columns
+NoFactor <- c("RNASeqEL")
+RNAEL5 <- droplevels(nofactorthese(RNAEL4, NoFactor))
+
+# Arrange into "DataType"
+RNAEL6 <- RNAEL5 %>% gather(contains("RNASeqEL"), key = "DataType", value = "Value")
+
+# Merge (rbind() here) and Write
+setwd("M:/Richard B/TCGA_data/DLBCL/processed_csv")
+write.csv(RNAEL6, "dlbcl_tcga_pancancer_rna.csv", row.names = F)
+
